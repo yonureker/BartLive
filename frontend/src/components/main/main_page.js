2d5afd665f43cb2a60d6ae027c47ce31fa0bcad0
yonureker <yonureker@gmail.com>
2019-07-17 10:11:17 -0700

    add react leaflet

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,13 +1,14 @@
 import React from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { Map, TileLayer, Marker, Popup, CircleMarker, LayersControl } from 'react-leaflet';
 
 class MainPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      stations: [],
+      stations: [{longitude: "-43.7833", latitude: "-5.3823"}],
       space_station: {longitude: "-43.7833", latitude: "-5.3823"},
       map : this.mymap,
       marker: this.circleMarker
@@ -17,46 +18,13 @@ class MainPage extends React.Component {
   componentWillMount() {
     this.props.fetchSpaceStation().then((response) => this.setState({space_station: response.space_station}));
     this.props.fetchStations().then((response) => this.setState({stations: response.stations}));
-    this.props.fetchRouteInfo();
+    this.props.fetchRouteInfo().then(response => this.setState({route_info: response.route_info}));
     this.props.fetchInitialStationDataSouth();
     this.props.fetchInitialStationDataNorth();
   }
 
   componentDidMount() {
-
-    
-    this.props.fetchRouteInfo().then(response => this.setState({ route_info: response}));
-    // this.mymap = L.map("map", {
-    //   center: [37.844443, -122.252341],
-    //   zoom: 11,
-    //   layers: [
-    //     L.tileLayer(
-    //       "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-    //       {
-    //         attribution: "Map data &copy;2012 Google",
-    //         maxZoom: 20
-    //       }
-    //     )
-    //   ]
-    // });
-
-    this.mymap = L.map("map", {
-      center: [37.844443, -122.252341],
-      zoom: 3,
-      layers: [
-        L.tileLayer(
-          "https://mt1.google.com/vt/lyrs=m@121,transit|vm:1&hl=en&opts=r&x={x}&y={y}&z={z}",
-          {
-            attribution: "Map data &copy;2012 Google",
-            maxZoom: 20
-          }
-        )
-      ]
-    });
-
-    // this.interval = setInterval(() => this.props.fetchSpaceStation(), 500)
-
-    // this.setState({ time: Date.now() }), 2000);
+    this.interval = setInterval(() => this.props.fetchSpaceStation(), 3000)
   }
 
   componentWillUnmount() {
@@ -65,46 +33,31 @@ class MainPage extends React.Component {
 
 
 
-  render() {
-    this.props.stations.map(el => {
-      return (this.circleMarker = L.circleMarker(
-        [el.gtfs_latitude, el.gtfs_longitude],
-        {
-          color: "blue",
-          fillColor: "blue",
-          fillOpacity: 0.7,
-          radius: 6,
-          opacity: 0.2
-        }
-      )).addTo(this.mymap);
-    });
+  render (){
+    const position = [37.844443, -122.252341];
+    // const customMarker = L.icon({ iconUrl: require('../../assets/images/iss.png')})
 
 
-    // this.state.map.removeLayer(this.state.marker);
+    if (this.props.stations.length === 0) {
+      return (
+        <div>Loading...</div>
+      )
+    } else {
+    return (
+      <Map center={position} zoom={11}>
+        <TileLayer
+          url="https://mt1.google.com/vt/lyrs=m@121,transit|vm:1&hl=en&opts=r&x={x}&y={y}&z={z}"
+        />
+        {this.props.stations.map((station, idx) => 
+          <CircleMarker key={`marker-${idx}`} center={[station.gtfs_latitude, station.gtfs_longitude]} radius={7}>
+          </CircleMarker>
+        )}
 
-      this.props.stations.map(el => {
-        return (this.circleMarker = L.circleMarker(
-          [this.props.space_station.latitude, this.props.space_station.longitude],
-          {
-            color: "blue",
-            fillColor: "blue",
-            fillOpacity: 0.7,
-            radius: 6,
-            opacity: 0.2
-          }
-        )).addTo(this.mymap);
-      });
-
-    const stations = (this.props.route_info.millbreaAntioch) ?
-     <div>{this.props.route_info["millbreaAntioch"].northbound.stations.EMBR.waypoints}</div> :
-    <div>Loading . . . </div>
-
-      return (<div>
-              <div id="map"/>
-              <div>{stations}</div>
-              {/* <div>{this.props.route_info["millbreaAntioch"]}</div> */}
-              </div>);
-
+          <CircleMarker key={10} center={[this.props.space_station.latitude, this.props.space_station.longitude]} radius={7}>
+          </CircleMarker>
+      </Map>
+    )
+    }
   }
 }
 
